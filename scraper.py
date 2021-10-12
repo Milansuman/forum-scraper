da65653843381387ca5a648d191152d6e3b04d3a
Milansuman <milansuman2016@gmail.com>
2021-10-12 20:11:21 +0530

    fixed virtual environment

--- scraper.py
+++ scraper.py
@@ -1,18 +1,22 @@
 import requests
+import gi
+from gi.repository import GTK
 from bs4 import BeautifulSoup
 
 forums = {
     "reddit": "http://libredd.it/search?q="
 }
 
-def main():
-    search = input("What do you want me to find: ")
+def find(search):
     response = requests.get(forums["reddit"]+search)
     soup = BeautifulSoup(response.content, "html.parser")
     for link in soup.find_all("a"):
         if "settings" in link.get("href") or "img" in link.get("href"):
             continue
         print(link.get("href"))
+
+def main():
+    pass
 
 if __name__ == "__main__":
     main()
