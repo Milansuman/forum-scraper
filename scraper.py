36983879b1dd4dfbf65a0de1df5d5b4ba858f231
Milansuman <milansuman2016@gmail.com>
2021-10-17 21:22:38 +0530

    moved everything into scraper class

--- scraper.py
+++ scraper.py
@@ -1,31 +1,38 @@
 import requests
-#import gi
-#gi.require_version("Gtk", "4.0")
-#from gi.repository import Gtk
 from bs4 import BeautifulSoup
 
-def find(search, filter):
-    response = requests.get(f"http://libredd.it/search?q={search}&sort=relevance")
-    soup = BeautifulSoup(response.content, "html.parser")
-    for link in soup.find_all("a"):
-        if "settings" in link.get("href") or "img" in link.get("href") or "github.com/" in link.get("href"):
-            continue
+class Scraper:
 
-        if filter in link.get("href"):
-            print(link.get("href"))
-        else:
-            continue
+    def __init__(self):
+        self.command = ""
+        self.filter = ""
+
+    def prompt(self):
+        self.command = input("> ")
+        if self.command in ("quit", "exit"):
+            exit()
+        elif self.command.split(" ")[0] == "search":
+            self.find(self.command.replace("search ", ""))
+        elif self.command.split(" ")[0] == "filter":
+            self.filter = "" if len(self.command.split(" ")) == 1 else self.command.split(" ")[1]
+
+    def find(self, search):
+        response = requests.get(f"http://libredd.it/search?q={search}&sort=relevance")
+        soup = BeautifulSoup(response.content, "html.parser")
+        for link in soup.find_all("a"):
+            if "settings" in link.get("href") or "img" in link.get("href") or "github.com/" in link.get("href"):
+                continue
+
+            if self.filter in link.get("href"):
+                print(link.get("href"))
+            else:
+                continue
 
 def main():
-    filter = ""
+    scraper = Scraper()
     while True:
-        command = input("> ")
-        if command in ("quit", "exit"):
-            break
-        elif command.split(" ")[0] == "search":
-            find(command.replace("search ", ""), filter)
-        elif command.split(" ")[0] == "filter":
-            filter = "" if len(command.split(" ")) == 1 else command.split(" ")[1]
+        scraper.prompt()
+        
 
 if __name__ == "__main__":
     main()
